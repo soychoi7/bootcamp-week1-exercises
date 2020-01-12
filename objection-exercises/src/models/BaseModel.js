7da42188dbee16a460819289224ba96ac01f73c6
Peter Bynum <pkpbynum@gmail.com>
2020-01-12 18:27:54 -0500

    snakeCaseMappers

--- objection-exercises/src/models/BaseModel.js
+++ objection-exercises/src/models/BaseModel.js
@@ -1,9 +1,12 @@
-const { Model } = require('objection')
+const { Model, snakeCaseMappers } = require('objection')
 const knex = require('../lib/index')
 
 Model.knex(knex)
 
 class BaseModel extends Model {
+  static get columnNameMappers() {
+    return snakeCaseMappers()
+  }
 
 }
 
